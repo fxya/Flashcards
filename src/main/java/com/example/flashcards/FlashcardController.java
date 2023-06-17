36e6e61032805edf07d25a3124dfdd0f60571dce
Faisal Ahsan <f.ahsan.07@gmail.com>
2023-06-17 06:34:48 +0100

    Got thymmleaf template rendering. Now to render a flashcard from db...

--- src/main/java/com/example/flashcards/FlashcardController.java
+++ src/main/java/com/example/flashcards/FlashcardController.java
@@ -1,11 +1,13 @@
 package com.example.flashcards;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
-@RestController
+@Controller
 class FlashcardController {
     private final FlashcardRepository repository;
 
@@ -14,11 +16,20 @@ class FlashcardController {
     }
 
     @GetMapping("/flashcards")
-    public List<Flashcard> getFlashcards() {
-        return (List<Flashcard>) repository.findAll();
+    public String getFlashcards(Model model) {
+        List<Flashcard> flashcards = (List<Flashcard>) repository.findAll();
+        model.addAttribute("flashcards", flashcards);
+        return "index";
     }
 
     // POST
+    @PostMapping("/flashcards")
+    public Flashcard createFlashcard(Flashcard flashcard) {
+        return repository.save(flashcard);
+    }
+
     // PUT
+
+    // GET INDIVIDUAL
     // DELETE INDIVIDUAL
 }
---
+++ src/main/resources/CreateTable.sql
@@ -0,0 +1,5 @@
+CREATE TABLE flashcard (
+    id SERIAL PRIMARY KEY,
+    question TEXT NOT NULL,
+    answer TEXT NOT NULL
+);
