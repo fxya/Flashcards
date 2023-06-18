d1a336ed4187cf75181f794c3e8aff49e5f8904a
Faisal Ahsan <f.ahsan.07@gmail.com>
2023-06-18 09:20:11 +0100

    Implement adding flashcard. Shuffle flashcards on each GET request using Collections.shuffle()

--- src/main/resources/static/flashcards.js
+++ src/main/resources/static/flashcards.js
@@ -9,6 +9,36 @@ function updateFlashcard() {
     }
 }
 
+function enableAddFlashcard() {
+    document.getElementById('question').outerHTML = '<input type="text" id="question" placeholder="Enter question">';
+    document.getElementById('answer').outerHTML = '<input type="text" id="answer" placeholder="Enter answer">';
+    document.getElementById('add').style.display = 'inline'; // Show the 'Add' button
+    document.getElementById('addFlashcard').style.display = 'none'; // Hide the 'Add Flashcard' button
+}
+
+function addFlashcard() {
+    const question = document.getElementById('question').value;
+    const answer = document.getElementById('answer').value;
+    const flashcard = {question: question, answer: answer};
+    fetch('api/flashcards', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(flashcard)
+    })
+        .then(response => response.json())
+        .then(data => {
+            flashcardList.push(data);
+            currentIndex = flashcardList.length - 1;
+            updateFlashcard();
+        })
+        .catch(error => console.error('Error:', error));
+    document.getElementById('question').outerHTML = '<p id="question"></p>'; // Convert back to p tag after adding
+    document.getElementById('answer').outerHTML = '<p id="answer"></p>'; // Convert back to p tag after adding
+    document.getElementById('add').style.display = 'none'; // Hide the 'Add' button
+    document.getElementById('addFlashcard').style.display = 'inline'; // Show the 'Add Flashcard' button
+    updateFlashcard();
+}
+
 function nextFlashcard() {
     if (currentIndex < flashcardList.length - 1) {
         currentIndex++;
